a585477ee600a657bc7fc466d4256d0dcd27465c
chennan <327919006@qq.com>
2019-07-17 11:18:08 +0800

    模块拆分

--- sso-server/src/main/java/com/cn/sso/server/config/SsoResourceServerConfig.java
+++ sso-server/src/main/java/com/cn/sso/server/config/SsoResourceServerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 @Configuration
 @EnableResourceServer
 public class SsoResourceServerConfig extends ResourceServerConfigurerAdapter {
+
     @Autowired
     protected AuthenticationSuccessHandler authenticationSuccessHandler;
     @Autowired
